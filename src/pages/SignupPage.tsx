32e5e0848550f026882115f5af67cba24cfa573a
Mark Salvino <mosalvino@gmail.com>
2026-02-23 13:08:17 -0800

    Removing unused data

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -16,7 +16,7 @@ const SignupPage: React.FC = () => {
     setError('');
     setSuccess('');
     // Sign up user with Supabase
-    const { data, error: signUpError } = await supabase.auth.signUp({
+    const { error: signUpError } = await supabase.auth.signUp({
       email,
       password,
       options: {
