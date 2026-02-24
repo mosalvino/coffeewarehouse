d4e99b901e545f832ff92bd836027beed73c262d
Mark Salvino <mosalvino@gmail.com>
2026-02-23 20:26:10 -0800

    refactor

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://sljesfeyqgsqlscfrgse.supabase.co';
-const supabaseKey = 'sb_publishable_lEGRPFyGdzby8Y6dXSjCFA_wRC0Zzc2';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
